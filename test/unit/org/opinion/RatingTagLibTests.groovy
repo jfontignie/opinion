18393cbd67da50528efa4f120c4dedf35bff6a62
jfontignie <jacques.fontignie@gmail.com>
2012-06-04 17:18:35 +0200

    first commit

---
+++ test/unit/org/opinion/RatingTagLibTests.groovy
@@ -0,0 +1,18 @@
+package org.opinion
+
+
+
+import grails.test.mixin.*
+
+import org.opinion.RatingTagLib
+
+/**
+ * See the API for {@link grails.test.mixin.web.GroovyPageUnitTestMixin} for usage instructions
+ */
+@TestFor(RatingTagLib)
+class RatingTagLibTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
